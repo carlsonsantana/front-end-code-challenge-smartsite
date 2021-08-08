b59430f04ca7fd1a37d58b9d1c78095addc7a23e
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-08-08 14:15:18 -0300

    Exibir o contador de resultados

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Counter() {
+  return (
+    <div className="counter-container">
+      Resultados encontrados: <strong>0</strong>
+    </div>
+  );
+}
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import HeaderForm from '../HeaderForm';
 import TrainingTime from '../TrainingTime';
 import ShowClosedUnits from '../ShowClosedUnits';
+import Counter from '../Counter';
 
 import './style.css';
 
@@ -11,7 +12,10 @@ export default function Form() {
     <div className="form">
       <HeaderForm />
       <TrainingTime />
-      <ShowClosedUnits />
+      <div className="same-line">
+        <ShowClosedUnits />
+        <Counter />
+      </div>
     </div>
   );
 }
